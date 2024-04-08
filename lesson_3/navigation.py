527322bfbd1aec77eb224c1da3447664d31f3aa6
Daria <podolskayad@gmail.com>
2024-04-08 22:54:51 +0400

    add lesson_3

---
+++ lesson_3/navigation.py
@@ -0,0 +1,26 @@
+import time
+
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+
+service = Service(executable_path=ChromeDriverManager().install())
+driver = webdriver.Chrome(service=service)
+
+driver.get("https://travel.yandex.ru/")
+
+time.sleep(3)
+
+driver.get("https://travel.yandex.ru/buses/")
+
+driver.back()
+
+time.sleep(3)
+
+driver.forward()
+
+time.sleep(3)
+
+driver.refresh()
+
+time.sleep(3)
